d77712cf3e5ca2b6c6950c7496154af0096c7a3f
masahirookubo <stmhamachiii@gmail.com>
2022-02-20 23:34:48 +0900

    controllerのテスト修正

--- golang/adapters/controllers/blogGet_test.go
+++ golang/adapters/controllers/blogGet_test.go
@@ -8,18 +8,28 @@ import (
 	controllers "clean_architecture/golang/adapters/controllers"
 	mock "clean_architecture/golang/adapters/uc.mock"
 
+	"gopkg.in/h2non/baloo.v3"
+
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestBlogsGetSuccess(t *testing.T) {
-	// var companyGetPath = "/api/blogs/1"
-
+	url := "/api/blogs/1"
+	// c, _ := gin.CreateTestContext(httptest.NewRecorder())
+	//
+	// blog := testData.Blog()
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
-
 	ucHandler := mock.NewMockHandler(mockCtrl)
+
+	// useCase := uc.GetBlogUseCase{
+	// 	OutputPort: json.NewPresenter(presenters.New(c)),
+	// 	InputPort:  uc.GetBlogParams{Id: blog.ID.Value},
+	// }
+
+	// TODO(okubo): useCaseを引数に入れたいが、address参照になっているので、一致せずに落ちる
+	// とはいえ、この辺りテストしないと意味ないので、ここは厳重に扱いたい
 	ucHandler.EXPECT().BlogGet(gomock.Any()).Times(1)
 	gE := gin.Default()
 
@@ -28,9 +38,12 @@ func TestBlogsGetSuccess(t *testing.T) {
 	ts := httptest.NewServer(gE)
 	defer ts.Close()
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodGet, "/api/blogs/1", nil)
-	gE.ServeHTTP(w, req)
-
-	assert.Equal(t, 200, w.Code)
+	if err := baloo.New(ts.URL).
+		Get(url).
+		Expect(t).
+		//JSONSchema(testData.CompanySingleRespDefinition).
+		Status(http.StatusOK).
+		Done(); err != nil {
+		t.Error(err)
+	}
 }
